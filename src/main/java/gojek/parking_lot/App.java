eb565385c80f880031fb2ece944cc4c4a2951db6
Gaurav Aggarwal <gaurav.agg83@gmail.com>
2020-03-21 18:46:21 +0530

    initial commit

---
+++ src/main/java/gojek/parking_lot/App.java
@@ -0,0 +1,13 @@
+package gojek.parking_lot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
